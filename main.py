4244e4663e4a2d6e032f741333ca36ec0cf70df7
alejes <alejes@yandex.ru>
2018-10-27 17:52:03 +0300

    add static method to save load file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from window.frames.welcome import Welcome
 
 def main():
     root = Tk()
-    root.geometry("850x500+300+300")
+    root.geometry("450x200+300+300")
     app = Welcome(root)
     root.mainloop()
 
