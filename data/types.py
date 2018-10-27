f9b97ecca539cebc976e712ba4e4075b777920e8
alejes <alejes@yandex.ru>
2018-10-27 15:29:21 +0300

    fix typing

--- data/types.py
+++ data/types.py
@@ -1,12 +1,12 @@
-from typing import List, Tuple
+from typing import List, Tuple, NamedTuple
 
-Point = Tuple[float, float]
-Color = Tuple[int, int, int]
-ColorPoint = Tuple[Point, Color]
+Point = NamedTuple('Point', [('x', float), ('y', float)])
+Color = NamedTuple('Color', [('r', int), ('g', int), ('b', int)])
+ColorPoint = NamedTuple('ColorPoint', [('point', Point), ('color', Color)])
 Bitmap = List[List[ColorPoint]]
 
-Interval = Tuple[int, int]
-Line = Tuple[Interval, Interval]
+Interval = NamedTuple('Interval', [('a', int), ('b', int)])
+Line = NamedTuple('Line', [('a', Interval), ('b', Interval)])
 
 Porosity = List[float]
 RockTypes = List[float]
