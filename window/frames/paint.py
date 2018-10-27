028d8bb801439268066b608699e7f5c7c207c9da
alejes <alejes@yandex.ru>
2018-10-28 02:38:56 +0300

    add 2 pictures:

--- window/frames/paint.py
+++ window/frames/paint.py
@@ -16,7 +16,7 @@ class Paint(Frame):
         self.master.title("Paint results")
         self.pack()
         self.draw_rock_type()
-        # self.draw_porosity(self.rockType.shape[0] + 25)
+        self.draw_porosity(self.rockType.shape[0] + 25)
 
     def draw_rock_type(self):
         image_grid = Label(self, text="Rock Type: ")
@@ -41,25 +41,29 @@ class Paint(Frame):
         self.canvas.create_oval(self.rockType.shape[1]+130, 210, self.rockType.shape[1]+130, 210, width=15, outline="#019f67")
         self.master.update()
 
-    def draw_porosity(self, start_height):
+    def draw_porosity(self, start):
         image_grid = Label(self, text="Porosity: ")
-        image_grid.place(x=0, y=start_height)
+        image_grid.place(x=0, y=start)
 
-        self.canvas = Canvas(self, width=self.rockType.shape[1]+300, height=self.rockType.shape[0])
-        self.canvas.place(x=100, y=0)
-        self.im = Image.frombytes('L', (self.rockType.shape[1], self.rockType.shape[0]), self.rockType.astype('b').tostring())
+        self.canvasPorosity = Canvas(self, width=self.porosity.shape[1] + 300, height=self.porosity.shape[0])
+        self.canvasPorosity.place(x=100, y=start)
+        self.imPorosity = Image.frombytes('L', (self.porosity.shape[1], self.porosity.shape[0]),
+                                  self.porosity.astype('b').tostring())
         # self.im = Image.fromarray(self.rockType)
         # print(self.im.mode)
         lut = []
-        lut.extend([255, 56, 20]) #ff3814
-        lut.extend([1, 159, 103]) #019f67
+        lut.extend([255, 56, 20])  # ff3814
+        lut.extend([1, 159, 103])  # 019f67
         lut.extend([0, 0, 0])  # black
-        self.im.putpalette(lut)
-        self.photo = ImageTk.PhotoImage(image=self.im)
-        self.canvas.create_image(0, 0, image=self.photo, anchor=NW)
-        self.canvas.create_text(600, 150, text="Legend:",justify=CENTER, font="Verdana 20")
-        self.canvas.create_text(600, 190, text="sandstone:", justify=LEFT, font="Verdana 14")
-        self.canvas.create_text(600, 210, text="shale:", justify=LEFT, font="Verdana 14")
-        self.canvas.create_oval(680, 190, 680, 190, width=15, outline="#ff3814")
-        self.canvas.create_oval(680, 210, 680, 210, width=15, outline="#019f67")
+        self.imPorosity.putpalette(lut)
+        self.photoPorosity = ImageTk.PhotoImage(image=self.imPorosity)
+        self.canvasPorosity.create_image(0, 0, image=self.photoPorosity, anchor=NW)
+        self.canvasPorosity.create_text(self.porosity.shape[1] + 60, 150, text="Legend:", justify=CENTER, font="Verdana 20")
+        self.canvasPorosity.create_text(self.porosity.shape[1] + 60, 190, text="sandstone:", justify=LEFT,
+                                font="Verdana 14")
+        self.canvasPorosity.create_text(self.porosity.shape[1] + 60, 210, text="shale:", justify=LEFT, font="Verdana 14")
+        self.canvasPorosity.create_oval(self.porosity.shape[1] + 130, 190, self.porosity.shape[1] + 130, 190, width=15,
+                                outline="#ff3814")
+        self.canvasPorosity.create_oval(self.porosity.shape[1] + 130, 210, self.porosity.shape[1] + 130, 210, width=15,
+                                outline="#019f67")
         self.master.update()
